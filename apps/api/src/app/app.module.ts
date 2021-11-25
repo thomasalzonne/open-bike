343f1125912e789c5d4d260a435123dccaa53516
Vincent GOUGEON <gougeonvincent31@outlook.fr>
2021-11-25 11:38:24 +0100

    feat(api): setup DB, env, typeorm

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+require('dotenv').config()
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.NX_DATABASE_HOST,
+      port: +process.env.NX_DATABASE_PORT || 3306,
+      username: process.env.NX_DATABASE_USER,
+      password: process.env.NX_DATABASE_PASS,
+      database: process.env.NX_DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
