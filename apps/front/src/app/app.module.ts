4df32bffbb558abafd0e930118fc3d395d19b34d
Vincent GOUGEON <gougeonvincent31@outlook.fr>
2021-12-02 14:54:55 +0100

    feat(front): map module

--- apps/front/src/app/app.module.ts
+++ apps/front/src/app/app.module.ts
@@ -6,15 +6,18 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router'
 
 const config: SocketIoConfig = { url: '/' };
 const routes: Routes = [
-  { path: '', component:  }
+  { path: '', component: HomeComponent },
+  { path: 'map', loadChildren: () => import('./map/map.module').then(m => m.MapModule) },
+  
 ];
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, HttpClientModule, SocketIoModule.forRoot(config), LayoutModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes), HttpClientModule, SocketIoModule.forRoot(config), LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- apps/front/src/app/map/map.module.ts
+++ apps/front/src/app/map/map.module.ts
@@ -1,15 +1,19 @@
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapComponent } from './map.component';
 
 
+const routes: Routes = [
+  { path: '', component: MapComponent}
+]
 
 @NgModule({
   declarations: [
     MapComponent
   ],
   imports: [
-    CommonModule
+    CommonModule, RouterModule.forChild(routes)
   ]
 })
 export class MapModule { }
